47ea6bec276e71394edef279c98272186711939b
Agata Blachowiak <lulacocoq@gmail..com>
2018-08-29 17:15:16 +0200

    home.blade.php logout button added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.master')
 @section('content')
+
     <div class="container">
         <div class="card">
             <div class="card-header">Twój status</div>
@@ -10,6 +11,24 @@
                     </div>
                 @endif
                 Już jesteś zalogowany!
+
+                <div class="logout-button">
+                    <a href="#" role="button">
+                        {{ Auth::user()->name }}
+                    </a>
+                    <button type="submit" class="log-reg-button">
+
+                        <a class="log-reg-button" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Wyloguj się') }}
+                        </a>
+                    </button>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+
             </div>
         </div>
     </div>
